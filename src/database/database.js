eb297330db470649db1bbe8ed8223acefb4b1e8a
Matheus Braga <sbmatheus20@gmail.com>
2026-01-17 17:05:29 -0300

    Removendo um ticket

--- src/database/database.js
+++ src/database/database.js
@@ -64,4 +64,16 @@ export class DataBase {
 
     console.log(rowIndex)
   }
+
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) =>
+      row.id === id)
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+
+    
+  }
 }
